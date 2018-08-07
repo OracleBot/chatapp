38dcb39f26b3345d322c2bed8a9b036a50f107d7
Verma <dipverma@deloitte.com>
2018-08-07 00:36:06 -0500

    Fixed HTML BOTUI

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var botui = new BotUI('api-bot');
+var botui = new BotUI('df-bot');
+
 const textarea = document.querySelector('.message-to-send');
 
 var socket = io.connect();
@@ -35,21 +36,22 @@ document.querySelector('.message-to-send').addEventListener('keyup', function (e
 socket.on('a_txt_reply', function (data) {
   console.log(`Client::a_txt_reply:response ${data.response}`);
   console.log(`Client::a_txt_reply:index ${data.msg_index}`);
-  //Remove
-  var x = data.response;
-  var bot_txt_reply_arr = x.split("<br>");
-  var y = 0;
-  for(let o=0; o<bot_txt_reply_arr.length ;o++){
-    botui.message.update(data.msg_index + 1, {
-      loading: false,
-      content: bot_txt_reply_arr[o]
-    });
-  }
-  // botui.message.update(data.msg_index + 1, {
-  //   loading: false,
-  //   content: data.response
-  // });
+  botui.message.update(data.msg_index + 1, {
+    loading: false,
+    content: data.response
+  });
 });
+
+// socket.on('a_txt_reply', function (data) {
+//   botui.message.add({
+//     content: 'Hello World from bot!<br /> after line break and a <a href="https://botui.org">link</a>. <br /> Another line <b>break</b>.'
+//   }).then(function () {
+//     return botui.action.button({
+//       delay: 1000,
+//       action: [{ text: 'Yes', value: 'yes' }, { text: 'No', value: 'no' }]
+//     });
+//   });
+// });
 
 socket.on('a_txt_loading', function (data) {
   botui.message.add({
@@ -64,6 +66,8 @@ socket.on('a_txt_reply_btn', function (data) {
   var btn_arr = {
     action: []
   };
+  console.log(`Btn Array ${btn_txt_arr}`);
+
   btn_txt_arr.map(function (item) {
     btn_arr.action.push({
       text: item,
@@ -76,6 +80,6 @@ socket.on('a_txt_reply_btn', function (data) {
   botui.action.button(btn_arr)
     .then(function (res) {
       textarea.disabled = false;
-      socket.emit('h_txt_query', { query: res.value, msg_index: data.msg_index+2});
+      socket.emit('h_txt_query', { query: res.value, msg_index: data.msg_index + 2 });
     });
 });
